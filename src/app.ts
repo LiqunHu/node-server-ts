7115e232a5519b9ac9a65d07710c5f986020cb9d
胡力群 <huliquns@126.com>
2022-02-24 17:29:44 +0800

    初始化项目

---
+++ src/app.ts
@@ -0,0 +1,47 @@
+import log4js from 'log4js'
+import express, { Request, Response } from 'express'
+import path from 'path'
+import cookieParser from 'cookie-parser'
+import bodyParser from 'body-parser'
+import cors from 'cors'
+
+const app = express()
+app.use(cors())
+
+app.use(express.static(path.join(__dirname, '../public')))
+app.use('/temp', express.static(path.join(__dirname, '../../public/temp')))
+app.use('/files', express.static(path.join(__dirname, '../../public/files')))
+app.use(
+  log4js.connectLogger(log4js.getLogger('http'), {
+    level: 'auto',
+    nolog: '\\.gif|\\.jpg$'
+  })
+)
+app.use(
+  bodyParser.json({
+    limit: '50mb'
+  })
+)
+app.use(
+  bodyParser.urlencoded({
+    extended: false
+  })
+)
+app.use(
+  bodyParser.text({
+    type: 'text/*'
+  })
+)
+app.use(bodyParser.raw())
+app.use(cookieParser())
+
+// 处理webpack服务请求
+app.get('/__webpack_hmr', function (req: Request, res: Response) {
+  res.send('')
+})
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Wellcome, My name is Hung.')
+})
+
+export default app
---
+++ src/index.ts
@@ -0,0 +1,54 @@
+#!/usr/bin/env node
+import http from 'http'
+import { AddressInfo } from 'net'
+import app from './app'
+
+const port = process.env.PORT || 9090
+
+app.set('port', port)
+
+/**
+ * Create HTTP server.
+ */
+
+const server = http.createServer(app)
+/**
+ * Event listener for HTTP server "error" event.
+ */
+
+const onError = (error: NodeJS.ErrnoException) => {
+  if (error.syscall !== 'listen') {
+    throw error
+  }
+
+  const bind = typeof port === 'string' ? 'Pipe ' + port : 'Port ' + port
+
+  // handle specific listen errors with friendly messages
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges')
+      process.exit(1)
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use')
+      process.exit(1)
+    default:
+      throw error
+  }
+}
+
+/**
+ * Event listener for HTTP server "listening" event.
+ */
+
+const onListening = () => {
+  const addr = server.address() as AddressInfo
+  const bind = 'port ' + addr.port
+  console.debug('Listening on ' + bind)
+}
+
+/**
+ * Listen on provided port, on all network interfaces.
+ */
+server.listen(port, () => {})
+server.on('error', onError)
+server.on('listening', onListening)
